cbb9fb4900e0189b29645379efa71f73d1ab34f0
ksrivast497 <80896578+ksrivast497@users.noreply.github.com>
2021-05-27 21:59:51 +0530

    Update Quadrilateral_area.java

--- ASSIGNMENT 1/Quadrilateral_area.java
+++ ASSIGNMENT 1/Quadrilateral_area.java
@@ -1,88 +1,96 @@
-class Quadrilateral
-{
-    protected double x1,x2,x3,x4,y1,y2,y3,y4;
-    protected void setCoordinate(double a,double b,double c,double d,double e,double f,double g,double h)
+import java.util.Scanner;
+
+public class Quadrilateral {
+    protected int x1,y1,x2,y2,x3,y3,x4,y4;
+    void points(int x1,int y1,int x2,int y2,int x3,int y3,int x4,int y4)
     {
-        x1=a;
-        y1=b;
-        x2=c;
-        y2=d;
-        x3=e;
-        y3=f;
-        x4=g;
-        y4=h;
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.x3 = x3;
+        this.y3 = y3;
+        this.x4 = x4;
+        this.y4 = y4;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter values for Square: ");
+        Square sq=new Square(sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt(),
+                sc.nextInt(),sc.nextInt());
+        System.out.println("Area Of The Square is " + sq.area());
+
+        Rectangle rec=new Rectangle(sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt(),
+                sc.nextInt(),sc.nextInt(),sc.nextInt());
+        System.out.println("Area Of The Rectangle is " + rec.area());
+
+        Parallelogram p=new Parallelogram(sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt(),
+                sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt(), sc.nextInt());
+        System.out.println("Area Of The Parallelogram is " + p.area());
+
+        Trapezoid t=new Trapezoid(sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt(),
+                sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextInt(), sc.nextInt());
+        System.out.println("Area Of The Trapezoid is " + t.area());
+    }
+}
+
+class Trapezoid extends Quadrilateral
+{
+    private final int height;
+    Trapezoid(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int i)
+    {
+        points(x1, y1, x2, y2, x3, y3, x4, y4);
+        height=i;
+    }
+    int area()
+    {
+        int d1=(int)Math.sqrt((x1-x2)(x1-x2)+(y1-y2)(y1-y2));
+        int d2=(int)Math.sqrt((x3-x4)(x3-x4)+(y3-y4)(y3-y4));
+        return ((d1+d2)*height)/2;
+    }
+}
+
+
+class Parallelogram extends Quadrilateral
+{
+    private final int height;
+    Parallelogram(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int i)
+    {
+        points(x1, y1, x2, y2, x3, y3, x4, y4);
+        height=i;
+    }
+    int area()
+    {
+        int d1=(int)Math.sqrt((x1-x2)(x1-x2)+(y1-y2)(y1-y2));
+        return d1*height;
     }
 }
 
 class Square extends Quadrilateral
 {
-    Square(double a,double b,double c,double d,double e,double f,double g,double h)
+    Square(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
     {
-        setCoordinate(a,b,c,d,e,f,g,h);
+        points(x1, y1, x2, y2, x3, y3, x4, y4);
     }
-    double area()
+    int area()
     {
-        double d=(double)Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+        int d=(int)Math.sqrt((x1-x2)*(x1-x2)*2);
         return d*d;
     }
 }
 
 class Rectangle extends Quadrilateral
 {
-    Rectangle(double a,double b,double c,double d,double e,double f,double g,double h)
+    Rectangle(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4)
     {
-        setCoordinate(a,b,c,d,e,f,g,h);
+        points(x1, y1, x2, y2, x3, y3, x4, y4);
     }
-    double area()
+    int area()
     {
-        double d1=(double)Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-        double d2=(double)Math.sqrt((x1-x4)*(x1-x4)+(y1-y4)*(y1-y4));
+        int d1=(int)Math.sqrt((x1-x2)(x1-x2)+(y1-y2)(y1-y2));
+        int d2=(int)Math.sqrt((x1-x4)(x1-x4)+(y1-y4)(y1-y4));
         return d1*d2;
-    }
-}
-
-class Trapezoid extends Quadrilateral
-{
-    private double height;
-    Trapezoid(double a,double b,double c,double d,double e,double f,double g,double h,double height)
-    {
-        setCoordinate(a,b,c,d,e,f,g,h);
-        this.height=height;
-    }
-    double area()
-    {
-        double d1=(double)Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-        double d2=(double)Math.sqrt((x3-x4)*(x3-x4)+(y3-y4)*(y3-y4));
-        return (double)((d1+d2)*height)/2;
-    }
-}
-
-class Parallelogram extends Quadrilateral
-{
-    private double height;
-    Parallelogram(double a,double b,double c,double d,double e,double f,double g,double h,double height)
-    {
-        setCoordinate(a,b,c,d,e,f,g,h);
-        this.height=height;
-    }
-    double area()
-    {
-        double d1=(double)Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-        return d1*height;
-    }
-}
-
-public class Quadrilateral_area
-{
-    public static void main(String []args)
-    {
-        Square sq=new Square(10.12,12.4,12.7,12.0,20,20,10,20);
-        System.out.println("Area Of The Square is " + sq.area());
-        Rectangle rec = new Rectangle(10.12,12.4,12.7,12.0,20,20,10,20);
-        System.out.println("Area Of The Rectangle is " + rec.area());
-        Parallelogram p = new Parallelogram(10,10,30,10,20,20,0,20,8);
-        System.out.println("Area Of The Parallelogram is " + p.area());
-        Trapezoid t = new Trapezoid(10,10,30,10,40,20,0,20,8);
-        System.out.println("Area Of The Trapezoid is " + t.area());
     }
 }
