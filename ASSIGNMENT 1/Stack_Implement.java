e8e1ddf386d84662e7c61869682132b6254852fa
ksrivast497 <80896578+ksrivast497@users.noreply.github.com>
2021-05-27 22:01:40 +0530

    Update Stack_Implement.java

--- ASSIGNMENT 1/Stack_Implement.java
+++ ASSIGNMENT 1/Stack_Implement.java
@@ -1,115 +1,143 @@
 import java.util.*;
+import java.io.*;
 
-class StackImp
+class Stack
 {
-    public void display(Stack st1,Stack st2)
+    Node head; //head of the list Stack
+    int max;
+    public int count = 0;
+    
+    //Node
+    static class Node
     {
-        if(st1.count()==st2.count())
+        int data;
+        Node next;
+        Node(int d)
         {
-            System.out.println("Total Numbers of Elements in both stack is : " + st2.count());
+            data = d;
+            next = null;
         }
     }
-    public static void main(String []args)
+
+    //Constructor
+    Stack(int i)
     {
-        Scanner sc = new Scanner(System.in);
-        Stack s1 = new Stack(20);
-        Stack s2 = new Stack(20);
+        max = i;
+    }
+
+    
+    boolean checkEmpty()
+    {
+        return count == 0;
+    }
+
+    boolean checkFull()
+    {
+        return count == max;
+    }
+    
+    
+    public void push(int data)
+    {
+        
+        if(checkFull())
+        {
+            System.out.println("Stack is Overflowing");
+        }
+       
+        else
+        {
+            Node node_new = new Node(data);
+            if(checkEmpty())
+            {
+                head = node_new;
+                count++;
+            }
+            else
+            {
+                node_new.next = head;
+                head = node_new;
+                count++;
+            }
+        }
+    }
+
+    public void pop()
+    {
+       
+        if(checkEmpty())
+        {
+            System.out.println("Stack is Underflowing");
+        }
+        
+        else
+        {
+            Node temp = head;
+            head = head.next;
+            count--;
+        }
+    }
+}
+
+public class StackImp
+{
+   
+    public void display(Stack st1, Stack st2)
+    {
+        if(st1.count==st2.count)
+        {
+            System.out.println("Total number of elements in both stacks is : "+st2.count);
+        }
+    }
+
+ 
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        Stack st1 = new Stack(20);
+        Stack st2 = new Stack(20);
         int ch, data;
         StackImp si = new StackImp();
         while(true)
         {
             System.out.println("\n1. Push in Stack 1");
-            System.out.println("2. Push in Stack 2");
-            System.out.println("3. Pop in Stack 1");
-            System.out.println("4. Pop in Stack 1");
-            System.out.println("5. Check Empty in Stack 1");
-            System.out.println("6. Check Empty in Stack 2");
-            System.out.println("Enter Your Choice : ");
+            System.out.println("\n2. Push in Stack 2");
+            System.out.println("\n3. Pop in Stack 1");
+            System.out.println("\n4. Pop in Stack 2");
+            System.out.println("\nEnter your choice");
             ch = sc.nextInt();
             switch(ch)
             {
                 case 1:
-                    System.out.println("Enter The Data");
-                    data = sc.nextInt();
-                    s1.push(data);
-                    si.display(s1,s2);
-                    break;
+                System.out.println("Enter the Data: ");
+                data = sc.nextInt();
+                st1.push(data);
+                si.display(st1,st2);
+                break;
+
                 case 2:
-                    System.out.println("Enter The Data");
-                    data=sc.nextInt();
-                    s2.push(data);
-                    si.display(s1,s2);
-                    break;
+                System.out.println("Enter the Data: ");
+                data = sc.nextInt();
+                st2.push(data);
+                si.display(st1,st2);
+                break;
+
                 case 3:
-                    System.out.println(s1.pop());
-                    si.display(s1,s2);
-                    break;
+                //System.out.println(s1.pop());
+                st1.pop();
+                si.display(st1,st2);
+                break;
+
                 case 4:
-                    System.out.println(s2.pop());
-                    si.display(s1,s2);
-                    break;
-                case 5:
-                    System.out.println(s1.checkEmpty());
-                    break;
-                case 6:
-                    System.out.println(s2.checkEmpty());
-                    break;
+                //System.out.println(s2.pop());
+                st2.pop();
+                si.display(st1,st2);
+                break;
+
                 default:
+                System.out.println("NULL");
+                break;
             }
         }
     }
 }
-
-class Stack
-{
-    private int[] s;
-    private int top, max;
-    Stack(int i)
-    {
-        max = i;
-        s = new int[max];
-        top = -1;
-    }
-    void push(int data)
-    {
-        if(top==max)
-        {
-            System.out.print("Stack is Overflow");
-        }
-        else
-        {
-            s[++top]=data;
-        }
-    }
-    int pop()
-    {
-        if(top==-1)
-        {
-            System.out.print("Stack is Underflow");
-            return 0;
-        }
-        else
-        {
-            return s[top--];
-        }
-    }
-    boolean checkEmpty()
-    {
-        if(max==-1)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    public int count()
-    {
-        if(top>-1)
-            return top+1;
-        else
-            return 0;
-    }
-}
-
