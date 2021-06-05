fe742ac75831cc8af105bd48bd93a0eeff7c86b8
ksrivast497 <80896578+ksrivast497@users.noreply.github.com>
2021-06-05 11:00:31 +0530

    Update frequencysort.java

--- Assignment3/frequencysort.java
+++ Assignment3/frequencysort.java
@@ -1,43 +1,51 @@
-package com.foxmula.Assignment3;
+package com.foxmula.Assignment_3;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-public class frequencysort {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of an array");
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        System.out.println("Enter numbers in an array");
-        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
-        for(int i=0;i<n;i++){
-            arr[i] = sc.nextInt();
-            if(hm.containsKey(arr[i])) hm.put(arr[i], hm.get(arr[i])+1);
-            else hm.put(arr[i], 1);
-        }
-        List<Map.Entry<Integer, Integer>> l = new ArrayList<>(hm.entrySet());
-        sort(l);
-        
-    }
-    
-    static void sort(List<Map.Entry<Integer, Integer>> l){
-        Collections.sort(l, new Comparator<Map.Entry<Integer, Integer>>() {
-            @Override
-            public int compare(Map.Entry<Integer, Integer> t, Map.Entry<Integer, Integer> t1) {
-                return t.getValue()-t1.getValue();
-            }
-        });
-        
-        for(Map.Entry<Integer, Integer> m : l){
-            System.out.println(m.getKey()+" : "+m.getValue());
-        }
-        
-        
-    }
+import java.util.*;
+import java.util.Map.Entry;
+
+public class frequencysort 
+{
+  void frequencysort(int ar[],int size)
+  {
+      HashMap<Integer,Integer> hashmap=new HashMap<Integer,Integer>();
+      for(int i=0;i<size;i++)
+      {
+          if(hashmap.containsKey(ar[i]))
+          {
+              hashmap.put(ar[i],hashmap.get(ar[i])+1);
+          }
+          else
+          {
+              hashmap.put(ar[i], 1);
+          }
+      }
+      Set<Entry<Integer,Integer>> set = hashmap.entrySet();
+      List<Entry<Integer,Integer>> list = new ArrayList<>(set);
+      Collections.sort(list,new Comparator<Entry<Integer,Integer>>()
+      {
+         public int compare(Map.Entry<Integer,Integer> o1, Map.Entry<Integer,Integer> o2) 
+         {
+             
+             return o1.getValue().compareTo(o2.getValue());
+         }
+      }); 
+      for (Map.Entry entry : hashmap.entrySet()) {
+          System.out.println(entry.getKey() + " " + entry.getValue());
+      }
+  }
+  public static void main(String[] args)
+  {
+      frequencysort obj=new frequencysort();
+      Scanner scanner=new Scanner(System.in);
+      System.out.println("Enter the size of the array ");
+      int n=scanner.nextInt();
+      int ar[]=new int[n];
+      System.out.println(" ENTER THE VALUES IN THE ARRAY ");
+      for(int i=0;i<n;i++)
+      {
+          ar[i]=scanner.nextInt();
+      }
+      obj.frequencysort(ar,n);
+      scanner.close();
+  }
 }
-
