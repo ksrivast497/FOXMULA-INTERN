d9c30191ce471f153a39d84eb0dba075f9bc350f
ksrivast497 <80896578+ksrivast497@users.noreply.github.com>
2021-06-05 10:48:25 +0530

    Update frequencysort.java

--- Assignment3/frequencysort.java
+++ Assignment3/frequencysort.java
@@ -1,45 +1,43 @@
-package com.FOXMULA.ASSIGNMENT3;
-import java.util.Arrays;
+package com.foxmula.Assignment3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-
-public class frequencysort
-{
-	public static void main(String[] args)
-	{
-        Scanner s = new Scanner(System.in);
-
-        System.out.println("enter length");
-        int x = s.nextInt();
-        Integer[] arr=new Integer[x];
-
-        System.out.println("enter the string");
-        for (int i = 0; i < x; i++)
-        {
-            arr[i]=s.nextInt();
+public class frequencysort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter numbers in an array");
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+            if(hm.containsKey(arr[i])) hm.put(arr[i], hm.get(arr[i])+1);
+            else hm.put(arr[i], 1);
         }
-        int count = 0;
-        System.out.println("enter the string to be checked");
-        int y = s.nextInt();
-
-        for (int i=0;i< arr.length;i++)
-        {
-           if (y==arr[i])
-           {
-              {
-                   count++;
-
-                }
-            }
-
-        }
-        System.out.println("no. of time of occurences:" + count);
-        System.out.println("sorted array is ");
-        Arrays.sort(arr);
-        for (int i = 0; i < arr.length; i++)
-        {
-            System.out.println(arr[i]);
-        }
+        List<Map.Entry<Integer, Integer>> l = new ArrayList<>(hm.entrySet());
+        sort(l);
+        
     }
-
+    
+    static void sort(List<Map.Entry<Integer, Integer>> l){
+        Collections.sort(l, new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> t, Map.Entry<Integer, Integer> t1) {
+                return t.getValue()-t1.getValue();
+            }
+        });
+        
+        for(Map.Entry<Integer, Integer> m : l){
+            System.out.println(m.getKey()+" : "+m.getValue());
+        }
+        
+        
+    }
 }
 
